85c480cc94b02df564fd81357e37cc6bb8b8a71e
masakiishida1022 <68501136+masakiishida1022@users.noreply.github.com>
2022-01-23 23:39:41 +0900

    開通！

--- public/RequestOrder.js
+++ public/RequestOrder.js
@@ -1,11 +1,16 @@
 // ボタンを押すと、ブロック内のプログラムが実行される
-console.log("JavaScriptを実行しています");
+//import axios from "axios";
+
 $("#btn1").on("click", function () {
+  axios
+    .get("http://localhost:9000/hello")
+    .then((response) => console.log("response body:", response.data));
+  //.then((response) => $("#tBox").val(response.data));
   let date = new Date();
   let year = date.getFullYear();
   let month = date.getMonth() + 1;
   let day = date.getDate();
   let message = `今日は${year}年${month}月${day}日です`;
   // jQueryを使って画面にメッセージを表示する
-  $("#tBox").val(message);
+  //$("#tBox").val(message);
 });
--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,9 @@
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+};
+
 exports.handler = function (event, context, callback) {
-  callback(null, { statusCode: 200, body: "hello, masaki" });
+  callback(null, { statusCode: 200, headers, body: "hello, masaki" });
 };
