a5a12a6b18d04e08363c7d7739de748ba5bb84f3
masakiishida1022 <68501136+masakiishida1022@users.noreply.github.com>
2022-01-25 00:33:08 +0900

    s

--- public/RequestOrder.js
+++ public/RequestOrder.js
@@ -4,8 +4,7 @@
 $("#btn1").on("click", function () {
   const text = document.getElementById("tBox").value;
   axios
-    //.get("http://localhost:9000/hello")
-    .get("/.netlify/functions/hello", JSON.parse(text))
+    .post("/.netlify/functions/hello", JSON.parse(text))
     //.then((response) => console.log("response body:", response.data));
     .then((response) => $("#outputBox").val(response.data));
 });
