69e433404596cccc72f17ddcfd3ad4c98eea41ab
masakiishida1022 <68501136+masakiishida1022@users.noreply.github.com>
2022-01-24 22:08:50 +0900

    Replyを受け取るTextBoxを修正

--- public/RequestOrder.js
+++ public/RequestOrder.js
@@ -6,12 +6,5 @@ $("#btn1").on("click", function () {
     //.get("http://localhost:9000/hello")
     .get("/.netlify/functions/hello")
     //.then((response) => console.log("response body:", response.data));
-    .then((response) => $("#tBox").val(response.data));
-  let date = new Date();
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-  let message = `今日は${year}年${month}月${day}日です`;
-  // jQueryを使って画面にメッセージを表示する
-  //$("#tBox").val(message);
+    .then((response) => $("#replyBox").val(response.data));
 });
