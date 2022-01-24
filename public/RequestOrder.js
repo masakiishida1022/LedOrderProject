7df13097298d5f2a8311744ff8956afab3b2d936
masakiishida1022 <68501136+masakiishida1022@users.noreply.github.com>
2022-01-24 22:04:55 +0900

    通信が可能に！

--- public/RequestOrder.js
+++ public/RequestOrder.js
@@ -5,8 +5,8 @@ $("#btn1").on("click", function () {
   axios
     //.get("http://localhost:9000/hello")
     .get("/.netlify/functions/hello")
-    .then((response) => console.log("response body:", response.data));
-  //.then((response) => $("#tBox").val(response.data));
+    //.then((response) => console.log("response body:", response.data));
+    .then((response) => $("#tBox").val(response.data));
   let date = new Date();
   let year = date.getFullYear();
   let month = date.getMonth() + 1;
