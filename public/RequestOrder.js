56d4abf05dd6405c1f5d89af7b0c4e298daaebfe
masakiishida1022 <68501136+masakiishida1022@users.noreply.github.com>
2022-01-24 22:17:13 +0900

    box再変更

--- public/RequestOrder.js
+++ public/RequestOrder.js
@@ -6,5 +6,5 @@ $("#btn1").on("click", function () {
     //.get("http://localhost:9000/hello")
     .get("/.netlify/functions/hello")
     //.then((response) => console.log("response body:", response.data));
-    .then((response) => $("#replyBox").val(response.data));
+    .then((response) => $("#outputBox").val(response.data));
 });
