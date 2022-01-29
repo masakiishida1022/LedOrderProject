bb29f721379e0588f8eb39330e20bdb3e30ccb9d
masakiishida1022 <68501136+masakiishida1022@users.noreply.github.com>
2022-01-30 00:51:08 +0900

    d

--- src/hello.js
+++ src/hello.js
@@ -8,15 +8,15 @@ exports.handler = function (event, context, callback) {
   //callback(null, { statusCode: 200, headers, body: "hello, masaki" });
   const body = JSON.parse(event.body);
 
-  const reply = {};
-  reply.length = 100;
+  const reply = { ...body };
+  /*reply.length = 100;
   reply.forEach((element, i) => {
     if (i < body.length) {
-      reply[String(i)] = body[String(i)];
+      reply[i] = body[i];
     } else {
-      reply[String(i)] = "0";
+      reply[i] = "0";
     }
-  });
+  });*/
 
   //const cpyBody = [...body];
 
