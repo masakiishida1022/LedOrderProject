9eb53182d73d59b0d0cc6a9583cf20414f0fa81b
masakiishida1022 <68501136+masakiishida1022@users.noreply.github.com>
2022-01-30 16:59:08 +0900

    d

--- src/lambdaFuncGetPattern.js
+++ src/lambdaFuncGetPattern.js
@@ -50,6 +50,6 @@ exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
     headers,
-    body: JSON.stringify(replyArray),
+    body: JSON.stringify(reply),
   });
 };
